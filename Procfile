1bff03d29a878fb8fbd2db03e0de6958128df921
ari10mi <ari10mi.programing@gmail.com>
2022-05-16 16:50:04 +0900

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JAVA_OPTS -jar build/libs/ticket-machine-0.0.1-SNAPSHOT.jar --server.port=$PORT
---
+++ src/test/java/com/ari3program/ticketmachine/TicketMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ari3program.ticketmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
