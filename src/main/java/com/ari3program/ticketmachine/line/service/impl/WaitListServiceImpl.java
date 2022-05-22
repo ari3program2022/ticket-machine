30e29b3211bcbccfcbf7bbdc684aafa4a10883b6
ari10mi <ari10mi.programing@gmail.com>
2022-05-22 23:40:29 +0900

    create waitlist test3

--- src/main/java/com/ari3program/ticketmachine/line/service/impl/WaitListServiceImpl.java
+++ src/main/java/com/ari3program/ticketmachine/line/service/impl/WaitListServiceImpl.java
@@ -1,9 +1,15 @@
 package com.ari3program.ticketmachine.line.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.ari3program.ticketmachine.line.entity.WaitList;
 import com.ari3program.ticketmachine.line.repository.WaitListRepository;
 import com.ari3program.ticketmachine.line.service.WaitListService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
 public class WaitListServiceImpl implements WaitListService {
 
 	private WaitListRepository waitListRepository;
