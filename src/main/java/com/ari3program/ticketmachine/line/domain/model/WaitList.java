2e7c0eec134a67a96885a2667b9620a759b1e35a
ari10mi <ari10mi.programing@gmail.com>
2022-05-24 11:59:31 +0900

    check my waitlist test2

--- src/main/java/com/ari3program/ticketmachine/line/domain/model/WaitList.java
+++ src/main/java/com/ari3program/ticketmachine/line/domain/model/WaitList.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -42,6 +44,7 @@ public class WaitList {
 	  @Column(name="amount")
 	  private int amount;
 
+	  @Enumerated(EnumType.STRING)
 	  @Column(name="status")
 	  private WaitListStatus status;
 
