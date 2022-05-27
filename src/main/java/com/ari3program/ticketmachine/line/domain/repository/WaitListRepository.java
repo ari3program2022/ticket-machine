e2644a78ce4dd534933e8877dc51a14bbc512233
ari10mi <ari10mi.programing@gmail.com>
2022-05-28 08:47:47 +0900

    get max value test8

--- src/main/java/com/ari3program/ticketmachine/line/domain/repository/WaitListRepository.java
+++ src/main/java/com/ari3program/ticketmachine/line/domain/repository/WaitListRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.ari3program.ticketmachine.line.domain.model.MaxReserveNo;
 import com.ari3program.ticketmachine.line.domain.model.WaitList;
 
 @Repository
